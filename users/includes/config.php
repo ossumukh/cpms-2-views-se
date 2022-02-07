148273b579f54edb1cb6204f78cb8a4947d23e22
Sumukh <osumukh.is19@rvce.edu.in>
2022-02-07 22:44:41 +0530

    commiting

---
+++ users/includes/config.php
@@ -0,0 +1,8 @@
+<?php
+$mysql_hostname = "localhost";
+$mysql_user = "ossum";
+$mysql_password = "focus";
+$mysql_database = "cms";
+$bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database");
+
+?>
